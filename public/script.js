ac446ad8f185c9d08dad6b7084e59323c71ac15c
yeeamanda <amanda.yee99@gmail.com>
2021-03-03 17:29:39 -0500

    changed rodent to category, spelling mistake

--- public/script.js
+++ public/script.js
@@ -1,10 +1,10 @@
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 const foodPlace = [];
 fetch(endpoint).then(blob => blob.json())
-.then(data => foodPlace.push(...data))
+.then(data => foodPlace.push(...data));
 
 
-function findMatches(searchQuery){
+function findMatches(searchQuery, foodPlace){
     return foodPlace.filter(place => {
         const regex = new RegExp(searchQuery, 'gi'); //regExp is an object that goes into .match method
         return place.city.match(regex) || place.category.match(regex)|| place.name.match(regex);
@@ -12,13 +12,13 @@ function findMatches(searchQuery){
 }
 
 function displayMatches(){
-    const matchArr = findMatches(this.value); //this.value is the data being input in the form
-    const html = matchArr.map(palce => { //.map creates an array with equal size but replaces the values with this instead
+    const matchArr = findMatches(this.value, foodPlace); //this.value is the data being input in the form
+    const html = matchArr.map(place => { //.map creates an array with equal size but replaces the values with this instead
         return `
         <li>
             <span class="name">${place.name}</span>
             <span class="name">${place.city}</span>
-            <span class="name">${place.rodent_and_insects}</span>
+            <span class="name">${place.category}</span>
         </li>
         `;
 
@@ -26,7 +26,7 @@ function displayMatches(){
 
     suggestions.innerHTML = html; //takes the html strong from html and creates html in this element
 
-}
+};
 
 const searchInput = document.querySelector('.search'); //This chooses an element with the class search
 const suggestions = document.querySelector('.suggestions'); //Chooses element with class suggestions
