4402cd72a3d8bd49426ee45dbfa93c22d9ed1d8f
LeToucanDev <34908969+LeToucanDev@users.noreply.github.com>
2021-03-05 17:53:07 -0800

    Baldric version that added all the asyncs and await to function properly. Also added Gradient for this code.

--- public/script.js
+++ public/script.js
@@ -1,10 +1,8 @@
-//async function windowActions(){
+async function windowActions(){
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-const request = await fetch(endpoint);
 const foodPlace = [];
-
-request.then(blob => blob.json())
-.then(data => foodPlace.push(...data))
+const request = await fetch(endpoint);
+const mArr = await request.json().then(data => foodPlace.push(...data));
 
 function findMatches(searchQuery, foodPlace){
     return foodPlace.filter(place => {
@@ -13,29 +11,33 @@ function findMatches(searchQuery, foodPlace){
     });
 };
 
-function displayMatches(){
-    const matchArr = findMatche(this.value, foodPlace); //this.value is the data being input in the form
+function displayMatches(event){
+    query = event.target.value;
+    const matchArr = findMatches(query, foodPlace); //this.value is the data being input in the form
     const html = matchArr.map(place => { //.map creates an array with equal size but replaces the values with this instead
         return `
 
-            <li class = "box has-background-danger-light m-2">
+            <li class = "sug-item box has-background-danger-light m-2">
                 <h1 class="name">${place.name}</h1>
-                <span class="name">${place.city} <\br></span>
+                <span class="name">${place.city} </br></span>
                 <span class="name">${place.category}</span>
             </li>
         `;
 
     }).join(''); //This changes html from an array to a big string
 
-    suggestions.innerHTML = html; //takes the html strong from html and creates html in this element
-
+    if(query){
+        suggestions.innerHTML = html; //takes the html strong from html and creates html in this element
+    }else {
+        suggestions.innerHTML = "";
+    }
 };
 
     const searchInput = document.querySelector('.search'); //This chooses an element with the class search
     const suggestions = document.querySelector('.suggestions'); //Chooses element with class suggestions
 
-    searchInput.addEventListener('change', displayMatches);
-    searchInput.addEventListener('keyup', displayMatches);
-//}
+    searchInput.addEventListener('change',(evt)=> displayMatches(evt));
+    searchInput.addEventListener('keyup',(evt)=> displayMatches(evt));
+}
 
-//window.onload = windowActions;
+window.onload = windowActions;
