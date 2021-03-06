dc256b47a6d6e5c5ab911aa0e4df126078679675
LeToucanDev <34908969+LeToucanDev@users.noreply.github.com>
2021-03-05 17:04:27 -0800

    Trying to check if things are working properly. The display is not Working and checking to see if files are fine.

--- public/script.js
+++ public/script.js
@@ -1,8 +1,10 @@
+//async function windowActions(){
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+const request = await fetch(endpoint);
 const foodPlace = [];
-fetch(endpoint).then(blob => blob.json())
-.then(data => foodPlace.push(...data))
 
+request.then(blob => blob.json())
+.then(data => foodPlace.push(...data))
 
 function findMatches(searchQuery, foodPlace){
     return foodPlace.filter(place => {
@@ -12,16 +14,15 @@ function findMatches(searchQuery, foodPlace){
 };
 
 function displayMatches(){
-    const matchArr = findMatches(this.value, foodPlace); //this.value is the data being input in the form
+    const matchArr = findMatche(this.value, foodPlace); //this.value is the data being input in the form
     const html = matchArr.map(place => { //.map creates an array with equal size but replaces the values with this instead
         return `
 
             <li class = "box has-background-danger-light m-2">
-                <span class="name">${place.name}</span>
-                <span class="name">${place.city}</span>
+                <h1 class="name">${place.name}</h1>
+                <span class="name">${place.city} <\br></span>
                 <span class="name">${place.category}</span>
             </li>
-
         `;
 
     }).join(''); //This changes html from an array to a big string
@@ -30,8 +31,11 @@ function displayMatches(){
 
 };
 
-const searchInput = document.querySelector('.search'); //This chooses an element with the class search
-const suggestions = document.querySelector('.suggestions'); //Chooses element with class suggestions
+    const searchInput = document.querySelector('.search'); //This chooses an element with the class search
+    const suggestions = document.querySelector('.suggestions'); //Chooses element with class suggestions
 
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+    searchInput.addEventListener('change', displayMatches);
+    searchInput.addEventListener('keyup', displayMatches);
+//}
+
+//window.onload = windowActions;
